bf5c5bb58340d3f81ae4820f5f66f3f3fa6b5cf8
sanjitram <24je0666@iitism.ac.in>
2025-07-20 00:07:12 +0530

    changes before frontend deployment

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
 // in production, there's no localhost so we have to make this dynamic
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api";
+// const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api";
+
+const BASE_URL = import.meta.env.PROD 
+  ? "https://mern-notes-backend-6kxk.onrender.com"  // Replace with your actual backend URL
+  : "http://localhost:5001/api";
 
 const api = axios.create({
   baseURL: BASE_URL,
